beff5a91e4a6809deff077c02229cf0a35086c15
seekdestiny <jifeiqian@gmail.com>
2016-05-05 21:28:36 -0700

    build main search function

--- java/Web_Crawler/src/com/netinstructions/crawler/Spider.java
+++ java/Web_Crawler/src/com/netinstructions/crawler/Spider.java
@@ -1,6 +1,9 @@
 package com.netinstructions.crawler;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Set;
+import java.util.List;
 
 public class Spider{
 	// Fields
@@ -8,6 +11,47 @@ public class Spider{
 	private Set<String> pagesVisited = new HashSet<String>();
 	private List<String> pagesToVisit = new LinkedList<String>();
 	
+	/**
+	   * Our main launching point for the Spider's functionality. Internally it creates spider legs
+	   * that make an HTTP request and parse the response (the web page).
+	   * 
+	   * @param url
+	   *            - The starting point of the spider
+	   * @param searchWord
+	   *            - The word or string that you are searching for
+	   */
+	public void search(String url, String searchWord) {
+		while (this.pagesVisited.size() < MAX_PAGES_TO_SEARCH) {
+			String currentUrl;
+			SpiderLeg leg = new SpiderLeg();
+			if (this.pagesToVisit.isEmpty()) {
+				currentUrl = url;
+				this.pagesVisited.add(url);
+			} else {
+				currentUrl = this.nextUrl();			
+			}
+			
+			leg.crawl(currentUrl); //Lots of stuff happening here.
+			                       //Look at the crawl method in SpiderLeg
+			boolean success = leg.searchForWord(searchWord);
+			if (success) {
+				System.out.println(String.format("**Success** Word %s found at %s", 
+						           searchWord, currentUrl));
+				break;
+			}
+			
+			this.pagesToVisit.addAll(leg.getLinks());		
+		}
+		
+		System.out.println(String.format("**Done** Visited %s web pages(s)", this.pagesVisited.size()));		
+	}	
+	
+	/**
+	   * Returns the next URL to visit (in the order that they were found). We also do a check to make
+	   * sure this method doesn't return a URL that has already been visited.
+	   * 
+	   * @return
+	   */
 	private String nextUrl() {
 		String nextUrl;
 		do {
@@ -17,5 +61,4 @@ public class Spider{
 		this.pagesVisited.add(nextUrl);
 		return nextUrl;
 	}
-	
 }
